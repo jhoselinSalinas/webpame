d3de07c8a2a4330a3f5f5c993b76a3c029193cca
alfredoSalinas <alfredosalinaszeballos@gmail.com>
2021-10-08 19:53:07 -0400

    cambios para probar

--- modal.js
+++ modal.js
@@ -1,5 +1,6 @@
 const boton = document.getElementById('boton')
 const btnModal = document.getElementById('btn-modal')
 boton.addEventListener('click',()=>{
+    alert('Hola Salinas')
     btnModal.checked = true
 })
